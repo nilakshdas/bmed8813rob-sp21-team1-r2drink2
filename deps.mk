0b89e2d8984dcddffd4e51cd8426d3ec06899977
Nilaksh Das <nilakshdas@gatech.edu>
2021-04-01 09:54:24 -0700

    Add makefiles

---
+++ deps.mk
@@ -0,0 +1,7 @@
+.PHONY: python_deps
+python_deps: $(PYENV_ROOT)
+	pip install --upgrade PyYAML
+	pip install gibson2 numpngw
+	pip install git+https://github.com/Healthcare-Robotics/assistive-gym.git
+	pip install git+https://github.com/Zackory/bullet3.git
+	yes | python -m gibson2.utils.assets_utils --download_ig_dataset
