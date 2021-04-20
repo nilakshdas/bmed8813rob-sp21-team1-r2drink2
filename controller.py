4c3ae5a555ab99fe7e16e4a6d82d92444cae6f74
Williams, David Nixon <dwilliams402@gatech.edu>
2021-04-20 12:01:34 -0400

    inputs

--- controller.py
+++ controller.py
@@ -5,8 +5,10 @@ from inputs import get_gamepad
 
 devices = DeviceManager()
 
+for device in devices:
+    print(device)
+
 while 1:
     events = get_gamepad()
     for event in events:
         print(event.ev_type, event.code, event.state)
-
