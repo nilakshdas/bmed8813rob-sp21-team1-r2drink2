50cca58f4f94172c376ef4e7e22a0c34f2891e2c
Nilaksh Das <nilakshdas@gatech.edu>
2021-04-26 19:56:27 -0700

    Add flag to use gamepad

--- run_teleop.py
+++ run_teleop.py
@@ -1,13 +1,14 @@
 import argparse
 
 from r2drink2.env import R2Drink2Env
-from r2drink2.teleop import KeyboardTeleOpController
+from r2drink2.teleop import GamepadTeleOpController, KeyboardTeleOpController
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-s", "--seed", type=int, default=42)
+    parser.add_argument("-g", "--gamepad", action="store_true")
 
     return parser.parse_args()
 
@@ -16,7 +17,10 @@ def main():
     args = parse_args()
 
     env = R2Drink2Env(render=True, seed=args.seed)
-    teleop_controller = KeyboardTeleOpController(env)
+
+    teleop_controller = (
+        GamepadTeleOpController(env) if args.gamepad else KeyboardTeleOpController(env)
+    )
 
     while True:
         teleop_controller.take_step()
