372c4c68dcf776d21819552cfa6aa93bc26bf134
Nilaksh Das <nilakshdas@gatech.edu>
2021-04-26 21:19:02 -0700

    Add different chair positions

--- r2drink2/setup/chair.py
+++ r2drink2/setup/chair.py
@@ -5,18 +5,27 @@ from assistive_gym.envs.env import AssistiveEnv
 
 
 CHAIR_Z_POS = 0.06
-CHAIR_POSITIONS = [np.array([2, 3, CHAIR_Z_POS])]
+CHAIR_POSITIONS = [
+    ("bedroom1", np.array([4, 4, CHAIR_Z_POS]), 0),
+    ("bedroom2", np.array([4, -4, CHAIR_Z_POS]), -np.pi),
+    ("bathroom", np.array([1, -4, CHAIR_Z_POS]), -np.pi / 2),
+    ("closet", np.array([-4, -3, CHAIR_Z_POS]), 0),
+    ("living", np.array([0, 4, CHAIR_Z_POS]), -np.pi / 2),
+    ("kitchen", np.array([-4, -1, CHAIR_Z_POS]), -np.pi),
+]
 
 
 def setup_chair(env: AssistiveEnv):
     env.furniture.init(
-        "wheelchair",
-        env.directory,
-        env.id,
-        env.np_random,
-        wheelchair_mounted=False,
+        "wheelchair", env.directory, env.id, env.np_random, wheelchair_mounted=False
     )
 
     _, orient = env.furniture.get_base_pos_orient()
-    pos = CHAIR_POSITIONS[env.np_random.choice(np.arange(len(CHAIR_POSITIONS)))]
+
+    room_index = env.np_random.choice(np.arange(len(CHAIR_POSITIONS)))
+    room, pos, yaw = CHAIR_POSITIONS[room_index]
+    orient = env.get_quaternion(env.get_euler(orient) + np.array([0, 0, yaw]))
+
     env.furniture.set_base_pos_orient(pos, orient)
+
+    print(room)
